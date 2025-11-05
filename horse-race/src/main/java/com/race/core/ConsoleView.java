f036171a2ef4d8da5d53d955fdf5c64700abff46
moonwonkim-a <happynei200@gmail.com>
2025-11-06 00:30:27 +0900

    feat : Bet class 구성

--- horse-race/src/main/java/com/race/core/ConsoleView.java
+++ horse-race/src/main/java/com/race/core/ConsoleView.java
@@ -1,4 +1,43 @@
 package main.java.com.race.core;
 
-public class ConsoleView {
+import main.java.com.race.model.bet.Bet;
+import main.java.com.race.model.card.Suit;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ConsoleView implements GameView{
+
+    private final Scanner sc = new Scanner(System.in);
+
+    @Override
+    public void println(String s) {
+        System.out.println(s);
+    }
+
+    @Override
+    public String readLine() {
+        return sc.nextLine().trim();
+    }
+
+    @Override
+    public void showStatus(Map<Suit, Integer> positions, int goal) {
+
+    }
+
+    @Override
+    public void showBets(List<Bet> bets) {
+
+    }
+
+    @Override
+    public void showWinner(Suit winner) {
+
+    }
+
+    @Override
+    public void showBalances(Map<String, Integer> balances) {
+
+    }
 }
--- horse-race/src/main/java/com/race/model/bet/Bet.java
+++ horse-race/src/main/java/com/race/model/bet/Bet.java
@@ -1,4 +1,34 @@
 package main.java.com.race.model.bet;
 
-public class Bet {
+/**
+ * 플레이어(또는 AI)의 한 번의 베팅 정보를 담는 클래스.
+ * - 불변(immutable) 객체로 설계되어야 함.
+ * - 단순 데이터 모델 역할만 수행 (로직 금지).
+ */
+public final class Bet {
+
+
+    private final String suit; // 배팅한 문양
+    private final int amount;  // 배팅금액
+
+    // 배팅정보 생성자
+    public Bet(String suit, int amount) {
+        this.suit = suit;
+        this.amount = amount;
+    }
+
+
+    public String getSuit() {
+        return suit;
+    }
+
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Bet] %s 무늬에 %d원", suit, amount);
+    }
 }
