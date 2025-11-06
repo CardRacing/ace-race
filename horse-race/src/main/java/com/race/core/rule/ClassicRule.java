b95e993ebd1fe595a78aef83017f7ad134ac83e2
soy <teabag759@gmail.com>
2025-11-06 17:36:33 +0900

    implement: ClassicRule 클래스 구현

--- horse-race/src/main/java/com/race/core/rule/ClassicRule.java
+++ horse-race/src/main/java/com/race/core/rule/ClassicRule.java
@@ -1,4 +1,25 @@
 package main.java.com.race.core.rule;
 
-public class ClassicRule {
+import main.java.com.race.model.race.Horse;
+import main.java.com.race.model.race.Track;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class ClassicRule implements RaceRule {
+    private static final int MIN = 7;
+    private static final int MAX = 10;
+
+    @Override
+    public int trackLength(Random rng) {
+        Objects.requireNonNull(rng, "Random cannot be null");
+        return rng.nextInt(MAX - MIN + 1) + MIN;
+    }
+
+    @Override
+    public boolean isFinish(Horse horse, Track track) {
+        Objects.requireNonNull(horse, "Horse cannot be null");
+        Objects.requireNonNull(track, "Track cannot be null");
+        return horse.position() >= track.goal();
+    }
 }
